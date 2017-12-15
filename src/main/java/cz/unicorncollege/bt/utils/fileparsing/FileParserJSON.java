543b17a6ececf5c59d2fbdd975ce3c9ba4f77126
DB-47 <DB-47@DB47_TischPC>
2017-12-15 02:39:37 +0100

    Metoda na JSON skoro hotová, ještě zbývá vyřešit problém s nevypsáním více než jednoho data

--- src/main/java/cz/unicorncollege/bt/utils/fileparsing/FileParserJSON.java
+++ src/main/java/cz/unicorncollege/bt/utils/fileparsing/FileParserJSON.java
@@ -9,15 +9,14 @@ import cz.unicorncollege.bt.model.MeetingCentre;
 import cz.unicorncollege.bt.model.MeetingRoom;
 import cz.unicorncollege.bt.model.Reservation;
 import cz.unicorncollege.bt.utils.Choices;
+import cz.unicorncollege.bt.utils.Convertors;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Date;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
@@ -28,12 +27,12 @@ import org.json.simple.JSONObject;
 public class FileParserJSON {
 
     /**
-     * Method to export data to JSON file
+     * Method to export inputData to JSON file
      *
-     * @param data Map containing all meeting centers, from whose will be
+     * @param inputData Map containing all meeting centers, from whose will be
      * extracted reservations and all necessary information
      */
-    public static void exportDataToJSON(Map<String, MeetingCentre> data) {
+    public static void exportDataToJSON(Map<String, MeetingCentre> inputData) {
         // TODO: ulozeni dat do souboru ve formatu JSON
         String locationFilter = Choices.getInput("Enter name of the file for export: ");
 
@@ -41,13 +40,11 @@ public class FileParserJSON {
         FileWriter fw = null;
         try {
             fw = new FileWriter(exportDataFile);
-            JSONObject main = new JSONObject();
-            JSONArray meetingObject = new JSONArray();
+            JSONObject main = new JSONObject(new LinkedHashMap());
+            JSONArray data = new JSONArray();
 
-            main.put("schema", "PLUS4U.EBC.MCS.MeetingRoom_Schedule_1.0\n");
-            main.put("uri", "ues:UCL-BT:UCL.INF/DEMO_REZERVACE:EBC.MCS.DEMO/MR001/SCHEDULE\n");
-            
-            for (Map.Entry<String, MeetingCentre> entry : data.entrySet()) {
+            for (Map.Entry<String, MeetingCentre> entry : inputData.entrySet()) {
+
                 String key = entry.getKey();
                 MeetingCentre value = entry.getValue();
                 for (Map.Entry<String, MeetingRoom> innerEntry : value.getMeetingRooms().entrySet()) {
@@ -55,11 +52,44 @@ public class FileParserJSON {
                     MeetingRoom innerValue = innerEntry.getValue();
                     //Pokud daný meeting room má nějakou rezervaci, provedeme výpis tohoto centra do JSONu
                     if (!innerValue.getReservations().isEmpty()) {
-                    //TODO: Implementuj zápis vnitřních dat
+                        JSONObject object = new JSONObject();
+                        object.put("meetingCentre", key);
+                        object.put("meetingRoom", innerKey);
+                        Map<Date, List<Reservation>> sortedReservations = innerValue.retrieveReservationSortedByDateAndByTime();
+
+                        for (Map.Entry<Date, List<Reservation>> innerEntry2 : sortedReservations.entrySet()) {
+
+                            Date date = innerEntry2.getKey();
+                            List<Reservation> reservations = innerEntry2.getValue();
+
+                            JSONObject retrievedDay = new JSONObject();
+                            JSONArray retrievedReservations = new JSONArray();
+
+                            String reservationDate = Convertors.convertDateToString(date);
+
+                            for (Reservation r : reservations) {
+                                JSONObject reservationRawData = new JSONObject();
+                                reservationRawData.put("from",r.getTimeFrom());
+                                reservationRawData.put("to",r.getTimeTo());
+                                reservationRawData.put("expectedPersonsCount",r.getExpectedPersonCount());
+                                reservationRawData.put("customer",Convertors.convertBooleanToWord(r.isNeedVideoConference()));
+                                reservationRawData.put("note",r.getNote());
+                                retrievedReservations.add(reservationRawData);
+                            }
+
+                            retrievedDay.put(reservationDate, retrievedReservations);
+                            object.put("reservations", retrievedDay);
+
+                        }
+                        
+                        data.add(object);
                     }
                 }
             }
-            main.put("data", meetingObject);
+
+            main.put("schema", "PLUS4U.EBC.MCS.MeetingRoom_Schedule_1.0");
+            main.put("uri", "ues:UCL-BT:UCL.INF/DEMO_REZERVACE:EBC.MCS.DEMO/MR001/SCHEDULE");
+            main.put("data", data);
             fw.write(main.toJSONString());
 
             fw.flush();
